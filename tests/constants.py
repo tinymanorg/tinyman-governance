ab5678240a6da2a20e2bd9e35509004436a4e873
etzellux <etzellux@gmail.com>
2023-11-16 11:49:32 +0300

    add arbitrary_executor_clear_state

--- tests/constants.py
+++ tests/constants.py
@@ -13,6 +13,8 @@ proposal_voting_clear_state_program = TealishProgram('contracts/proposal_voting/
 rewards_approval_program = TealishProgram('contracts/rewards/rewards_approval.tl')
 rewards_clear_state_program = TealishProgram('contracts/rewards/rewards_clear_state.tl')
 
+arbitrary_executor_approval_program = TealishProgram('contracts/arbitrary_executor/arbitrary_executor_approval.tl')
+arbitrary_executor_clear_state_program = TealishProgram('contracts/arbitrary_executor/arbitrary_executor_clear_state.tl')
 
 TINY_ASSET_ID = 12345
 
@@ -20,3 +22,4 @@ VAULT_APP_ID = 6000
 REWARDS_APP_ID = 7000
 STAKING_VOTING_APP_ID = 8000
 PROPOSAL_VOTING_APP_ID = 9000
+ARBITRARY_EXECUTOR_APP_ID = 10000
