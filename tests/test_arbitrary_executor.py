dee010a05b457bcca0b615b83007a684b2971b9e
etzellux <etzellux@gmail.com>
2023-11-17 18:14:45 +0300

    add test_execute_proposal

--- tests/test_arbitrary_executor.py
+++ tests/test_arbitrary_executor.py
@@ -43,7 +43,8 @@ from tinyman.utils import TransactionGroup, bytes_to_int, int_to_bytes
 
 from tests.common import BaseTestCase, ProposalVotingAppMixin, VaultAppMixin, ArbitraryExecutorAppMixin
 from tests.constants import (PROPOSAL_VOTING_APP_ID, TINY_ASSET_ID,
-                             VAULT_APP_ID, proposal_voting_approval_program,
+                             VAULT_APP_ID, ARBITRARY_EXECUTOR_APP_ID,
+                             proposal_voting_approval_program,
                              proposal_voting_clear_state_program,
                              arbitrary_executor_approval_program,
                              arbitrary_executor_clear_state_program)
@@ -70,6 +71,22 @@ class ArbitraryExecutorTestCase(VaultAppMixin, ProposalVotingAppMixin, Arbitrary
         self.create_vault_app(self.manager_address)
         self.init_vault_app(self.vault_app_creation_timestamp + 30)
         self.create_proposal_voting_app(self.manager_address)
+    
+    def assert_on_check_proposal_state(self, proposal_id, expected_state, sender, sender_sk, block_timestamp):
+        txn_group = prepare_get_proposal_state_transactions(
+            proposal_voting_app_id=PROPOSAL_VOTING_APP_ID,
+            sender=sender,
+            proposal_id=proposal_id,
+            suggested_params=self.sp,
+        )
+        txn_group.sign_with_private_key(sender, sender_sk)
+        
+        block = self.ledger.eval_transactions(txn_group.signed_transactions, block_timestamp)
+        app_call_txn = get_first_app_call_txn(block[b'txns'])
+        logs = app_call_txn[b'dt'][b'lg']
+        self.assertEqual(len(logs), 1)
+        proposal_state = logs[0][4:]
+        self.assertEqual(bytes_to_int(proposal_state), expected_state)
 
     def test_create_app(self):
         block_datetime = datetime(year=2022, month=3, day=2, tzinfo=ZoneInfo("UTC"))
@@ -100,3 +117,292 @@ class ArbitraryExecutorTestCase(VaultAppMixin, ProposalVotingAppMixin, Arbitrary
                 b'proposal_voting_app_id': PROPOSAL_VOTING_APP_ID,
             }
         )
+    
+    def test_execute_proposal(self):
+        user_sk, user_address = generate_account()
+        user_2_sk, user_2_address = generate_account()
+        user_3_sk, user_3_address = generate_account()
+        user_4_sk, user_4_address = generate_account()
+
+        self.ledger.set_account_balance(user_address, 10_000_000)
+        self.ledger.set_account_balance(user_2_address, 10_000_000)
+        self.ledger.set_account_balance(user_3_address, 10_000_000)
+        self.ledger.set_account_balance(user_4_address, 10_000_000)
+        self.create_proposal_voting_app(self.manager_address, self.proposal_manager_address)
+        self.ledger.global_states[PROPOSAL_VOTING_APP_ID][b'quorum_threshold'] = 7_000_000
+        self.ledger.set_account_balance(get_application_address(PROPOSAL_VOTING_APP_ID), proposal_voting.constants.PROPOSAL_VOTING_APP_MINIMUM_BALANCE_REQUIREMENT)
+
+        block_timestamp = self.vault_app_creation_timestamp + 2 * WEEK
+        self.create_checkpoints(user_address, user_sk, block_timestamp)
+
+        # Create lock 1
+        lock_end_timestamp = get_start_timestamp_of_week(block_timestamp) + 15 * WEEK
+        amount = 100_000_000
+        self.ledger.move(
+            amount,
+            asset_id=TINY_ASSET_ID,
+            sender=self.ledger.assets[TINY_ASSET_ID]["creator"],
+            receiver=user_address
+        )
+
+        with unittest.mock.patch("time.time", return_value=block_timestamp):
+            txn_group = prepare_create_lock_transactions(
+                vault_app_id=VAULT_APP_ID,
+                tiny_asset_id=TINY_ASSET_ID,
+                sender=user_address,
+                locked_amount=amount,
+                lock_end_time=lock_end_timestamp,
+                vault_app_global_state=get_vault_app_global_state(self.ledger),
+                account_state=get_account_state(self.ledger, user_address),
+                slope_change_at_lock_end_time=get_slope_change_at(self.ledger, lock_end_timestamp),
+                suggested_params=self.sp,
+            )
+        txn_group.sign_with_private_key(user_address, user_sk)
+        self.ledger.eval_transactions(txn_group.signed_transactions, block_timestamp=block_timestamp)
+        slope = get_slope(amount)
+        user_1_bias = get_bias(slope, (lock_end_timestamp - block_timestamp))
+
+        #  Create lock 2
+        lock_end_timestamp = get_start_timestamp_of_week(block_timestamp) + 5 * WEEK
+        amount = 10_000_000
+        self.ledger.move(
+            amount,
+            asset_id=TINY_ASSET_ID,
+            sender=self.ledger.assets[TINY_ASSET_ID]["creator"],
+            receiver=user_2_address
+        )
+
+        with unittest.mock.patch("time.time", return_value=block_timestamp):
+            txn_group = prepare_create_lock_transactions(
+                vault_app_id=VAULT_APP_ID,
+                tiny_asset_id=TINY_ASSET_ID,
+                sender=user_2_address,
+                locked_amount=amount,
+                lock_end_time=lock_end_timestamp,
+                vault_app_global_state=get_vault_app_global_state(self.ledger),
+                account_state=get_account_state(self.ledger, user_2_address),
+                slope_change_at_lock_end_time=get_slope_change_at(self.ledger, lock_end_timestamp),
+                suggested_params=self.sp,
+            )
+
+        txn_group.sign_with_private_key(user_2_address, user_2_sk)
+        self.ledger.eval_transactions(txn_group.signed_transactions, block_timestamp=block_timestamp)
+        slope = get_slope(amount)
+        user_2_bias = get_bias(slope, (lock_end_timestamp - block_timestamp))
+
+        # Create lock 3
+        lock_end_timestamp = get_start_timestamp_of_week(block_timestamp) + 5 * WEEK
+        amount = 20_000_000
+        self.ledger.move(
+            amount,
+            asset_id=TINY_ASSET_ID,
+            sender=self.ledger.assets[TINY_ASSET_ID]["creator"],
+            receiver=user_3_address
+        )
+
+        with unittest.mock.patch("time.time", return_value=block_timestamp):
+            txn_group = prepare_create_lock_transactions(
+                vault_app_id=VAULT_APP_ID,
+                tiny_asset_id=TINY_ASSET_ID,
+                sender=user_3_address,
+                locked_amount=amount,
+                lock_end_time=lock_end_timestamp,
+                vault_app_global_state=get_vault_app_global_state(self.ledger),
+                account_state=get_account_state(self.ledger, user_3_address),
+                slope_change_at_lock_end_time=get_slope_change_at(self.ledger, lock_end_timestamp),
+                suggested_params=self.sp,
+            )
+        txn_group.sign_with_private_key(user_3_address, user_3_sk)
+        self.ledger.eval_transactions(txn_group.signed_transactions, block_timestamp=block_timestamp)
+        slope = get_slope(amount)
+        user_3_bias = get_bias(slope, (lock_end_timestamp - block_timestamp))
+        
+        # Create lock 4
+        lock_end_timestamp = get_start_timestamp_of_week(block_timestamp) + 5 * WEEK
+        amount = 10_000_000
+        self.ledger.move(
+            amount,
+            asset_id=TINY_ASSET_ID,
+            sender=self.ledger.assets[TINY_ASSET_ID]["creator"],
+            receiver=user_4_address
+        )
+
+        with unittest.mock.patch("time.time", return_value=block_timestamp):
+            txn_group = prepare_create_lock_transactions(
+                vault_app_id=VAULT_APP_ID,
+                tiny_asset_id=TINY_ASSET_ID,
+                sender=user_4_address,
+                locked_amount=amount,
+                lock_end_time=lock_end_timestamp,
+                vault_app_global_state=get_vault_app_global_state(self.ledger),
+                account_state=get_account_state(self.ledger, user_4_address),
+                slope_change_at_lock_end_time=get_slope_change_at(self.ledger, lock_end_timestamp),
+                suggested_params=self.sp,
+            )
+        txn_group.sign_with_private_key(user_4_address, user_4_sk)
+        self.ledger.eval_transactions(txn_group.signed_transactions, block_timestamp=block_timestamp)
+        slope = get_slope(amount)
+        user_4_bias = get_bias(slope, (lock_end_timestamp - block_timestamp))
+
+        arbitrary_transaction = transaction.AssetConfigTxn(
+            sender=user_address,
+            sp=self.sp,
+            total=1000,
+            default_frozen=False,
+            unit_name="TINYRING",
+            asset_name="Tiny Ring",
+            manager=user_address,
+            reserve=user_address,
+            freeze=user_address,
+            clawback=user_address,
+            url="https://ipfs.io/ipfs/RANDOM_CID", 
+            decimals=0,
+        )
+
+        # Create proposal
+        proposal_id = generate_cid_from_proposal_metadata({"name": "Proposal 1"})
+        txn_group = prepare_create_proposal_transactions(
+            proposal_voting_app_id=PROPOSAL_VOTING_APP_ID,
+            vault_app_id=VAULT_APP_ID,
+            sender=user_address,
+            proposal_id=proposal_id,
+            execution_hash=arbitrary_transaction.get_txid(),
+            vault_app_global_state=get_vault_app_global_state(self.ledger),
+            suggested_params=self.sp
+        )
+        txn_group.sign_with_private_key(user_address, user_sk)
+        block = self.ledger.eval_transactions(txn_group.signed_transactions, block_timestamp=block_timestamp)
+
+        proposal_box_name = get_proposal_box_name(proposal_id)
+        proposal = parse_box_proposal(self.ledger.boxes[PROPOSAL_VOTING_APP_ID][proposal_box_name])
+
+        # Approve proposal
+        txn_group = prepare_approve_proposal_transactions(
+            proposal_voting_app_id=PROPOSAL_VOTING_APP_ID,
+            sender=self.proposal_manager_address,
+            proposal_id=proposal_id,
+            suggested_params=self.sp
+        )
+        txn_group.sign_with_private_key(self.proposal_manager_address, self.proposal_manager_sk)
+        block = self.ledger.eval_transactions(txn_group.signed_transactions, block_timestamp=block_timestamp)
+
+        proposal = parse_box_proposal(self.ledger.boxes[PROPOSAL_VOTING_APP_ID][proposal_box_name])
+
+        # Cast Vote
+        proposal_creation_timestamp = proposal.creation_timestamp
+        block_timestamp = proposal.voting_start_timestamp
+
+        with unittest.mock.patch("time.time", return_value=proposal.voting_start_timestamp):
+            self.assertEqual(proposal.state, proposal_voting.constants.PROPOSAL_STATE_ACTIVE)
+            self.assert_on_check_proposal_state(proposal_id, proposal_voting.constants.PROPOSAL_STATE_ACTIVE, user_address, user_sk, block_timestamp=proposal.voting_start_timestamp)
+
+        # User 4
+        vote = 1
+        txn_group = prepare_cast_vote_transactions(
+            proposal_voting_app_id=PROPOSAL_VOTING_APP_ID,
+            vault_app_id=VAULT_APP_ID,
+            sender=user_4_address,
+            proposal_id=proposal_id,
+            proposal=proposal,
+            vote=vote,
+            account_power_index=get_account_power_index_at(self.ledger, VAULT_APP_ID, user_4_address, proposal_creation_timestamp),
+            create_attendance_sheet_box=True,
+            suggested_params=self.sp,
+        )
+        txn_group.sign_with_private_key(user_4_address, user_4_sk)
+        self.ledger.eval_transactions(txn_group.signed_transactions, block_timestamp=block_timestamp)
+
+        proposal = parse_box_proposal(self.ledger.boxes[PROPOSAL_VOTING_APP_ID][proposal_box_name])
+        self.assertEqual(proposal.against_voting_power, 0)
+        self.assertEqual(proposal.for_voting_power, user_4_bias)
+        self.assertEqual(proposal.abstain_voting_power, 0)
+        self.assertEqual(proposal.is_quorum_reached, False)
+
+        # User 2
+        vote = 0
+        txn_group = prepare_cast_vote_transactions(
+            proposal_voting_app_id=PROPOSAL_VOTING_APP_ID,
+            vault_app_id=VAULT_APP_ID,
+            sender=user_2_address,
+            proposal_id=proposal_id,
+            proposal=proposal,
+            vote=vote,
+            account_power_index=get_account_power_index_at(self.ledger, VAULT_APP_ID, user_2_address, proposal_creation_timestamp),
+            create_attendance_sheet_box=True,
+            suggested_params=self.sp,
+        )
+        txn_group.sign_with_private_key(user_2_address, user_2_sk)
+        self.ledger.eval_transactions(txn_group.signed_transactions, block_timestamp=block_timestamp)
+
+        proposal = parse_box_proposal(self.ledger.boxes[PROPOSAL_VOTING_APP_ID][proposal_box_name])
+        self.assertEqual(proposal.against_voting_power, user_2_bias)
+        self.assertEqual(proposal.for_voting_power, user_4_bias)
+        self.assertEqual(proposal.abstain_voting_power, 0)
+        self.assertEqual(proposal.is_quorum_reached, False)
+
+        # User 3
+        vote = 2
+        txn_group = prepare_cast_vote_transactions(
+            proposal_voting_app_id=PROPOSAL_VOTING_APP_ID,
+            vault_app_id=VAULT_APP_ID,
+            sender=user_3_address,
+            proposal_id=proposal_id,
+            proposal=proposal,
+            vote=vote,
+            account_power_index=get_account_power_index_at(self.ledger, VAULT_APP_ID, user_3_address, proposal_creation_timestamp),
+            create_attendance_sheet_box=True,
+            suggested_params=self.sp,
+        )
+        txn_group.sign_with_private_key(user_3_address, user_3_sk)
+        self.ledger.eval_transactions(txn_group.signed_transactions, block_timestamp=block_timestamp)
+        
+        proposal = parse_box_proposal(self.ledger.boxes[PROPOSAL_VOTING_APP_ID][proposal_box_name])
+        self.assertEqual(proposal.against_voting_power, user_2_bias)
+        self.assertEqual(proposal.for_voting_power, user_4_bias)
+        self.assertEqual(proposal.abstain_voting_power, user_3_bias)
+        self.assertEqual(proposal.is_quorum_reached, False)
+        
+        with unittest.mock.patch("time.time", return_value=proposal.voting_end_timestamp + 10):
+            self.assert_on_check_proposal_state(proposal_id, proposal_voting.constants.PROPOSAL_STATE_DEFEATED, user_address, user_sk, block_timestamp=proposal.voting_end_timestamp + 10)
+
+        # User 1
+        vote = 1
+        txn_group = prepare_cast_vote_transactions(
+            proposal_voting_app_id=PROPOSAL_VOTING_APP_ID,
+            vault_app_id=VAULT_APP_ID,
+            sender=user_address,
+            proposal_id=proposal_id,
+            proposal=proposal,
+            vote=vote,
+            account_power_index=get_account_power_index_at(self.ledger, VAULT_APP_ID, user_address, proposal_creation_timestamp),
+            create_attendance_sheet_box=True,
+            suggested_params=self.sp,
+        )
+        txn_group.sign_with_private_key(user_address, user_sk)
+        block = self.ledger.eval_transactions(txn_group.signed_transactions, block_timestamp=block_timestamp)
+
+        proposal = parse_box_proposal(self.ledger.boxes[PROPOSAL_VOTING_APP_ID][proposal_box_name])
+        
+        with unittest.mock.patch("time.time", return_value=proposal.voting_end_timestamp + 10):
+            self.assert_on_check_proposal_state(proposal_id, proposal_voting.constants.PROPOSAL_STATE_SUCCEEDED, user_address, user_sk, block_timestamp=proposal.voting_end_timestamp + 10)
+
+        # Execute proposal
+        self.create_arbitrary_executor_app(self.manager_address)
+
+        self.sp.fee = 10_000
+        executor_txn = transaction.ApplicationNoOpTxn(
+            sender=user_address,
+            sp=self.sp,
+            index=ARBITRARY_EXECUTOR_APP_ID,
+            app_args=["validate_transaction", proposal_id],
+            foreign_apps=[PROPOSAL_VOTING_APP_ID],
+            boxes=[
+                (PROPOSAL_VOTING_APP_ID, proposal_box_name)
+            ],
+        )
+        txn_group = TransactionGroup([
+            executor_txn,
+            arbitrary_transaction
+        ])
+        txn_group.sign_with_private_key(user_address, user_sk)
+        block = self.ledger.eval_transactions(txn_group.signed_transactions, block_timestamp=proposal.voting_end_timestamp + 10)
